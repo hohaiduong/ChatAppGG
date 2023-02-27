36257ca3679c7d2991f4c3a6b46f5a9c7b5743e4
hohaiduong <loyalist092@gmail.com>
2023-02-27 18:34:07 +0700

    Add Chat
    Add Chat

--- src/components/navigator/Navigation.js
+++ src/components/navigator/Navigation.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+// import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import ScreenLogin from '../View/loginScreen.js';
-
+import ScreenLogin from '../View/loginScreen';
+import HomeChat from '../View/Home';
+import AddChat from '../View/AddChat';
+import ChatRoom from '../View/ChatRoom';
 const Stack = createNativeStackNavigator();
 
 const StackNavigation = () => {
 
     return(
-        <Stack.Navigator>
+        <Stack.Navigator screenOptions={{headerShown: false}}>
             <Stack.Screen name='Login' component={ScreenLogin}></Stack.Screen>
+            <Stack.Screen name='Home' component={HomeChat}></Stack.Screen>
+            <Stack.Screen name='Search' component={AddChat}></Stack.Screen>
+            <Stack.Screen name='ChatRoom' component={ChatRoom}></Stack.Screen>
         </Stack.Navigator>
     )
 }
