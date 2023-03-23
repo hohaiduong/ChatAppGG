e0401dfbf7779ea1e755cbffcb083c5c59a93b3e
hohaiduong <loyalist092@gmail.com>
2023-03-23 17:57:13 +0700

    Create Splash

--- index.js
+++ index.js
@@ -3,7 +3,24 @@
  */
 
 import {AppRegistry} from 'react-native';
+import { Component } from 'react';
+import Splash from './src/components/assets/splash/Splash';
 import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+class Main extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {currentScreen : 'Splash'};
+        setTimeout(() => {
+            this.setState({ currentScreen: 'App' })
+        }, 4000);
+    }
+    render() {
+        const { currentScreen } = this.state;
+        let mainScreen = currentScreen === 'Splash' ? <Splash/> : <App/>
+        return mainScreen
+    }
+
+}
+AppRegistry.registerComponent(appName, () => Main);
